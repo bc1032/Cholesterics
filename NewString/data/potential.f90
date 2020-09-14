a11104b20c134625bd64f69a3511c056d5a5ca2d
bc1032 <33351629+bc1032@users.noreply.github.com>
2020-09-14 14:04:34 +0100

    Add files via upload

--- NewString/data/potential.f90
+++ NewString/data/potential.f90
@@ -32,17 +32,162 @@ CONTAINS
   end subroutine perturb_coordinates
 
 !Compute the energy, energy derivatives and gradient (if called for)
-Subroutine IMPLEMENT_POTENTIAL(COORDS2,V,E,GTEST) 
+Subroutine IMPLEMENT_POTENTIAL(COORDS2,V,E,GTEST)
   IMPLICIT NONE
   LOGICAL GTEST
-  DOUBLE PRECISION COORDS2(N), E
+  INTEGER :: I, J, K, Cur, lx, ly, lz
+  DOUBLE PRECISION COORDS2(N), E, BULK, SPLAY, SURFACE, q_0, WS, TWIST, A, B, C, ks, kt, gridsize, s
   DOUBLE PRECISION :: V(N)
-  
+  DOUBLE PRECISION, ALLOCATABLE :: Q1(:,:), Q2(:,:), Q3(:,:), weight(:,:)
+  DOUBLE PRECISION, ALLOCATABLE :: Q4(:,:), Q5(:,:), BOUNDARY(:)
+  DOUBLE PRECISION, ALLOCATABLE :: Qt1(:,:), Qt2(:,:), Qt3(:,:), Qt4(:,:), Qt5(:,:)
+  DOUBLE PRECISION, ALLOCATABLE :: GRADXQ1(:,:), GRADXQ2(:,:)
+  DOUBLE PRECISION, ALLOCATABLE :: GRADXQ3(:,:), GRADXQ4(:,:), GRADXQ5(:,:)
+  DOUBLE PRECISION, ALLOCATABLE :: GRADZQ1(:,:), GRADZQ2(:,:)
+  DOUBLE PRECISION, ALLOCATABLE :: GRADZQ3(:,:), GRADZQ4(:,:), GRADZQ5(:,:)
+
+  !Allocate array memory for all dimensions; length of lx, lz
+  ALLOCATE(weight(lz,lx))
+  ALLOCATE(Q1(lz,lx))
+  ALLOCATE(Q2(lz,lx))
+  ALLOCATE(Q3(lz,lx))
+  ALLOCATE(Q4(lz,lx))
+  ALLOCATE(Q5(lz,lx))
+  ALLOCATE(GRADXQ1(lz,lx))
+  ALLOCATE(GRADXQ2(lz,lx))
+  ALLOCATE(GRADXQ3(lz,lx))
+  ALLOCATE(GRADXQ4(lz,lx))
+  ALLOCATE(GRADXQ5(lz,lx))
+  ALLOCATE(GRADZQ1(lz,lx))
+  ALLOCATE(GRADZQ2(lz,lx))
+  ALLOCATE(GRADZQ3(lz,lx))
+  ALLOCATE(GRADZQ4(lz,lx))
+  ALLOCATE(GRADZQ5(lz,lx))
+  !Initialise array values for completeness
+  weight(:,:) = 0.0D0
+  GRADZQ1(:,:) = 0.0D0
+  GRADZQ2(:,:) = 0.0D0
+  GRADZQ3(:,:) = 0.0D0
+  GRADZQ4(:,:) = 0.0D0
+  GRADZQ5(:,:) = 0.0D0
+  GRADXQ1(:,:) = 0.0D0
+  GRADXQ2(:,:) = 0.0D0
+  GRADXQ3(:,:) = 0.0D0
+  GRADXQ4(:,:) = 0.0D0
+  GRADXQ5(:,:) = 0.0D0
+  Q1(:,:) = 0.0D0
+  Q2(:,:) = 0.0D0
+  Q3(:,:) = 0.0D0
+  Q4(:,:) = 0.0D0
+  Q5(:,:) = 0.0D0
+
+  ws = 0.01D0
+  bulk = 0.0D0
+  splay = 0.0D0
+  twist = 0.0D0
+  surface = 0.0D0
   E = 0.0
   V(:) = 0.0
+  lx = 30
+  ly = 1
+  lz = 30
+  gridsize = 1.0D0 / (lz)
+  A = 0.3D0
+  B = 0.2D0
+  C = 1.0D0
+  ks = 1.0D0
+  kt = 1.0D0
+  ! q_0 = (((WINDNUMin+WINDNUMfin)*PI))/(2.0D0*(lz))
+  ! a_0 = (((WINDNUMin)*PI))/((lz))
+  ! bulkan = -((b**4) + 36*a*c*(b**2) + 216*(a**2)*(c**2) + (b**3)*SQRT((b**2) + 24*a*c) + 24*a*b*c*SQRT((b**2) + 24*a*c))&
+  !      / (864 * (c**3) )
+  ! twistan = (ly)*(kt*s**2/3.0D0)*(3*a_0**2-6*a_0*q_0+4*q_0**2)
+  ! surfacean = ((b + SQRT(b**2 + 24*a*c))**2 * (lx)*(ly)*(SIN(pi*WINDNUMin))**2 )/ (8*c**2)
 
-  TOYX=COORDS2(1)
-  TOYY=COORDS2(2)
+  s = (B + DSQRT(B**2 + 24D0*A*C))/(4.0D0*C)
+
+!In the following loop we use central difference formulae (explicit method) for finite differences.
+  do k = 1,lz
+    do i = 1,lx
+      Q1(k,i)=s*(COS(COORDS2(i + ((k-1)*lx)))*COS(COORDS2(i + ((k-1)*lx)))-(1.0D0/3.0D0))
+      Q2(k,i)=s*(COS(COORDS2(i + ((k-1)*lx)))*COS(COORDS2(i + ((k-1)*lx))))
+      Q3(k,i) = 0.0D0
+      Q4(k,i) = s*(SIN(COORDS2(i + ((k-1)*lx)))*SIN(COORDS2(i + ((k-1)*lx)))-(1.0D0/3.0D0))
+      Q5(k,i) = 0.0D0
+
+      IF ( (K .EQ. lz) .OR. (K .EQ. 1) ) THEN
+        !WEIGHT(K,I) = 0.5 * PFGRIDSIZE**3
+        WEIGHT(K,I) = 0.5D0*gridsize**2
+      ELSE
+        !WEIGHT(K,I) = 1.0 * PFGRIDSIZE**3
+        WEIGHT(K,I) = 1.0D0*gridsize**2
+      end if
+
+      bulk = bulk + weight(k,i)*(-a*(Q1(k,i)*Q4(k,i) + Q1(k,i)**2 + Q2(k,i)**2 + Q3(k,i)**2 + Q4(k,i)**2 + Q5(k,i)**2) &
+            + b*(Q1(k,i)*(-Q2(k,i)**2 + Q4(k,i)**2 + Q5(k,i)**2) + (Q1(k,i)**2)*Q4(k,i) - 2*Q2(k,i)*Q3(k,i)*Q5(k,i) &
+            - (Q2(k,i)**2)*Q4(k,i) + (Q3(k,i)**2)*Q4(k,i)) &
+            + c*(Q1(k,i)*Q4(k,i) + Q1(k,i)**2 + Q2(k,i)**2 + Q3(k,i)**2 + Q4(k,i)**2 + Q5(k,i)**2)**2 )
+
+      IF (k .EQ. lz) THEN
+        GRADZQ1(k,i)   =  (WS/gridsize) * (3*Q1(k,i)-4*Q1(k-1,i)+Q1(k-2,i)) / (2.0D0)
+        GRADZQ2(k,i)   =  (WS/gridsize) * (3*Q2(k,i)-4*Q2(k-1,i)+Q2(k-2,i)) / (2.0D0)
+        GRADZQ3(k,i)   =  (WS/gridsize) * (3*Q3(k,i)-4*Q3(k-1,i)+Q3(k-2,i)) / (2.0D0)
+        GRADZQ4(k,i)   =  (WS/gridsize) * (3*Q4(k,i)-4*Q4(k-1,i)+Q4(k-2,i)) / (2.0D0)
+        GRADZQ5(k,i)   =  (WS/gridsize) * (3*Q5(k,i)-4*Q5(k-1,i)+Q5(k-2,i)) / (2.0D0)
+
+        surface = surface + (weight(k,i)/gridsize)*0.5D0*Ws*( (Q1(k,i)-Qt1(k,i))**2 + 2*(Q2(k,i)-Qt2(k,i))**2 &
+                + 2*(Q3(k,i)-Qt3(k,i))**2+(Qt1(k,i) + Qt4(k,i) - Q1(k,i) - Q4(k,i))**2 &
+                + (Q4(k,i) - Qt4(k,i))**2 +2*(Q5(k,i) - Qt5(k,i))**2 )
+      ELSEIF (k .EQ. 1) THEN
+        GRADZQ1(k,i)   =  (WS/gridsize) * (-3*Q1(k,i)+4*Q1(k+1,i)-Q1(k+2,i)) / (2.0D0)
+        GRADZQ2(k,i)   =  (WS/gridsize) * (-3*Q2(k,i)+4*Q2(k+1,i)-Q2(k+2,i)) / (2.0D0)
+        GRADZQ3(k,i)   =  (WS/gridsize) * (-3*Q3(k,i)+4*Q3(k+1,i)-Q3(k+2,i)) / (2.0D0)
+        GRADZQ4(k,i)   =  (WS/gridsize) * (-3*Q4(k,i)+4*Q4(k+1,i)-Q4(k+2,i)) / (2.0D0)
+        GRADZQ5(k,i)   =  (WS/gridsize) * (-3*Q5(k,i)+4*Q5(k+1,i)-Q5(k+2,i)) / (2.0D0)
+        surface = surface + (weight(k,i)/gridsize)*0.5D0*Ws*( (Q1(k,i)-Qt1(k,i))**2 + 2*(Q2(k,i)-Qt2(k,i))**2 &
+                + 2*(Q3(k,i)-Qt3(k,i))**2+(Qt1(k,i) + Qt4(k,i) - Q1(k,i) - Q4(k,i))**2 &
+                + (Q4(k,i) - Qt4(k,i))**2 +2*(Q5(k,i) - Qt5(k,i))**2 )
+      ELSE
+        !Calculate dz components.
+        GRADZQ1(k,i) = (Q1(k+1,i) - Q1(k-1,i)) / (2.0D0)
+        GRADZQ2(k,i) = (Q2(k+1,i) - Q2(k-1,i)) / (2.0D0)
+        GRADZQ3(k,i) = (Q3(k+1,i) - Q3(k-1,i)) / (2.0D0)
+        GRADZQ4(k,i) = (Q4(k+1,i) - Q4(k-1,i)) / (2.0D0)
+        GRADZQ5(k,i) = (Q5(k+1,i) - Q5(k-1,i)) / (2.0D0)
+      ENDIF
+      IF (i == lx) THEN
+        !Here we enforce periodicity in the x-direction (vertical).
+        GRADXQ1(k,i) = (Q1(k,1) - Q1(k,i-1)) / (2.0D0 )
+        GRADXQ2(k,i) = (Q2(k,1) - Q2(k,i-1)) / (2.0D0 )
+        GRADXQ3(k,i) = (Q3(k,1) - Q3(k,i-1)) / (2.0D0 )
+        GRADXQ4(k,i) = (Q4(k,1) - Q4(k,i-1)) / (2.0D0 )
+        GRADXQ5(k,i) = (Q5(k,1) - Q5(k,i-1)) / (2.0D0 )
+      ELSEIF (i == 1) THEN
+        GRADXQ1(k,i) = (Q1(k,i+1) - Q1(k,lx+1)) / (2.0D0 )
+        GRADXQ2(k,i) = (Q2(k,i+1) - Q2(k,lx+1)) / (2.0D0 )
+        GRADXQ3(k,i) = (Q3(k,i+1) - Q3(k,lx+1)) / (2.0D0 )
+        GRADXQ4(k,i) = (Q4(k,i+1) - Q4(k,lx+1)) / (2.0D0 )
+        GRADXQ5(k,i) = (Q5(k,i+1) - Q5(k,lx+1)) / (2.0D0 )
+      ELSE
+        !Calculate dx components for divergence squared (splay) term
+        GRADXQ1(k,i) = GRADXQ1(k,i) + (Q1(k,i+1) - Q1(k,i-1)) / (2.0D0)
+        GRADXQ2(k,i) = GRADXQ2(k,i) + (Q2(k,i+1) - Q2(k,i-1)) / (2.0D0)
+        GRADXQ3(k,i) = GRADXQ3(k,i) + (Q3(k,i+1) - Q3(k,i-1)) / (2.0D0)
+        GRADXQ4(k,i) = GRADXQ4(k,i) + (Q4(k,i+1) - Q4(k,i-1)) / (2.0D0)
+        GRADXQ5(k,i) = GRADXQ5(k,i) + (Q5(k,i+1) - Q5(k,i-1)) / (2.0D0)
+
+      ENDIF
+
+      twist = twist + (weight(k,i)/gridsize)*0.5D0*kt*( (-2*q_0*Q1(k,i)+gradzQ2(k,i))**2 &
+            + 2*(2*q_0*Q3(k,i) - gradzQ5(k,i))*(2*q_0*Q3(k,i)+gradxQ2(k,i)) &
+            + 2*(2*q_0*Q2(k,i)-gradzQ4(k,i))*(2*q_0*Q2(k,i)+gradzQ1(k,i) - gradxQ3(k,i)) &
+            + 2*(2*q_0*Q5(k,i)+gradxQ4(k,i))*(2*q_0*Q5(k,i)+gradzQ3(k,i)+gradxQ1(k,i)-gradxQ4(k,i)) &
+            + (2*q_0*Q4(k,i)+gradzQ2(k,i)-GRADXQ5(k,i))**2 + (-2*q_0*(Q1(k,i)+Q4(k,i)) + GRADXQ5(k,i))**2)
+
+      splay = splay + 0.5D0*(weight(k,i)/gridsize)*ks*((gradzQ3(k,i)+gradxQ1(k,i))**2 &
+            + ( gradzQ5(k,i) + gradxQ2(k,i) )**2 + (gradzQ1(k,i) + gradzQ4(k,i) - gradxQ3(k,i))**2 )
+    enddo
+  enddo
 
   CALL COMPUTE_ENERGY(E)
 
@@ -58,9 +203,9 @@ END SUBROUTINE IMPLEMENT_POTENTIAL
 
 SUBROUTINE COMPUTE_ENERGY(E)
   IMPLICIT NONE
-  DOUBLE PRECISION :: E
-  E = 1.0D-7*(TOYX**4+TOYY**4) - 2.0D-4*(TOYX**3+TOYY**3) + 0.1442*(TOYX**2+TOYY**2) + 10924.6 &
-     & -47.18*TOYX - 46.58*TOYY + 5.0D-3*TOYX*TOYY
+  DOUBLE PRECISION :: E, bulk, twist, splay, surface
+  E = BULK + TWIST + SPLAY + SURFACE
+
 END SUBROUTINE COMPUTE_ENERGY
 
 
